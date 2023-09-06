2edde48eace6596cbc25b27c5f1b31300d7c83c4
Administrator <Administrator@VINAY-IT>
2023-09-06 16:31:17 +0530

    Working for Fare Detail Page

--- OnionConsumeWebAPI/Extensions/AppUrlConstant.cs
+++ OnionConsumeWebAPI/Extensions/AppUrlConstant.cs
@@ -6,6 +6,7 @@
         public static string BaseURL = "http://localhost:5225/";
 
         public static string URLAirasia = "https://dotrezapi.test.I5.navitaire.com";
+        public static string URLAirasia22 = "https://dotrezapi.test.I5.navitaire.com";
 
 
     }
